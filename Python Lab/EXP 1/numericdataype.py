d00de50517cbf0125ec3730ecb8c8128f6d3fa81
Vishal Mahajan <vism06@gmail.com>
2023-12-14 02:00:00 +0530

    Initial

---
+++ Python Lab/EXP 1/numericdataype.py
@@ -0,0 +1,11 @@
+"""
+Numeric Data Type in Python are int, float, complex
+"""
+
+int_num = 10
+float_num = 10.5
+complex_num = 1+2j
+
+print(f"int_num: {int_num}")
+print(f"float_num: {float_num}")
+print(f"complex_num: {complex_num}")
